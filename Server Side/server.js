7a67775b2bd3c6467e304d220ad9f6045342cf92
Abinesh-Srinivasan <abineshsrinivasan1301@gmail.com>
2024-07-20 11:39:23 +0530

    Developer own code for post and get request

--- Server Side/server.js
+++ Server Side/server.js
@@ -2,13 +2,15 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 
-// Connecting to the DataBase
-mongoose
-  .connect("mongodb://localhost:27017")
-  .then(() => console.log("MongoDb Connected"))
-  .catch((err) => console.log(err));
+// Database Connection
+const connectionString = "mongodb://localhost:27017/";
+async function mongoDbConnect() {
+  await mongoose.connect(connectionString);
+  console.log("Database connected");
+}
+mongoDbConnect().catch((err) => console.log(`Db Connection error:${err}`));
 
-// Creating Schema
+// Creating MongoDb Schema
 const taskSchema = {
   title: {
     type: String,
@@ -18,34 +20,37 @@ const taskSchema = {
 };
 
 // Creating Model for Schema
-const taskModel = mongoose.model("Task", taskSchema);
+const taskModel = new mongoose.model("Task", taskSchema);
 
-// Middleware to parse JSON data
+// Middleware to Parse JSON Data
 app.use(express.json());
 
-// Creating new item in the Db
-app.post("/tasks", async (req, res) => {
-  const { title, description } = req.body;
+// Creating the task in the Db
+app.post("/task", async (req, res) => {
   try {
-    const newTask = new taskModel({ title, description });
-    await newTask.save();
+    const { title, description } = req.body;
+    const newTask = await taskModel({ title, description });
+    newTask.save();
     res.status(201).json(newTask);
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ message: err.message });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: `${error.message}` });
   }
 });
 
-// Get all items from Db
-app.get("/tasks", async (req, res) => {
+// Obtaining all the tasks from the Db
+app.get("/task", async (req, res) => {
   try {
     const allTasks = await taskModel.find();
-    res.status(200).json(allTasks);
+    res.status(200).send(allTasks);
   } catch (error) {
     console.log(error);
-    res.status(500).json({ message: error.message });
+    res.status(500).json({ message: `${error.message}` });
   }
 });
 
 // Server connection
-app.listen(5000, () => console.log("Server connected to the port 5000."));
+const portNumber = 5000;
+app.listen(portNumber, () =>
+  console.log(`Server connected to the port ${portNumber}...`)
+);
